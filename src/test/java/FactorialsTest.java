3ed834c59d2bc8d148befd7fbfd1e39040933c1a
matthew-holmes <MHolmes1@qa.com>
2021-05-17 10:59:10 +0100

    MH: Factorial Tests done (100% coverage)

--- src/test/java/FactorialsTest.java
+++ src/test/java/FactorialsTest.java
@@ -1,3 +1,21 @@
+import org.junit.BeforeClass;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class FactorialsTest {
+
+    Factorials f = new Factorials();
+
+    @BeforeClass
+    public static void setup(){
+
+    }
+
+    @Test
+    public void reverseFactorialTest(){
+        assertEquals(3,f.reverseFactorial(6));
+        assertEquals(5,f.reverseFactorial(120));
+        assertEquals(-1,f.reverseFactorial(150));
+    }
 }
